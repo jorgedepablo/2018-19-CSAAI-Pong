7f8729f70237f3324629ffdbeba90c4ba46aa9d7
Jorge De Pablo Martinez <jdepablo@beta03.aulas.gsyc.urjc.es>
2019-03-14 12:58:03 +0100

    en proceso

--- pong-01.js
+++ pong-01.js
@@ -1,6 +1,105 @@
-function main()
-{
-    console.log("Pong: Main: Start!")
+function field (w, h){
+    this.ctx = null;
+    this.width = w;
+    this.height = h;
+
+    this.init = function(ctx) {
+        this.ctx = ctx;
+    };
+
+    this.draw = function(points1, points2){
+        this.ctx.beginPath();
+        this.ctx.strokeStyle = "white";
+        this.ctx.setLineDash([5, 5]);
+        this.ctx.moveTo(this.width/2,0);
+        this.ctx.lineTo(this.width/2, this.height)
+        this.ctx.stroke();
+
+        this.ctx.font = "80px Arial";
+        this.ctx.fillStyle = 'white';
+        this.ctx.fillText(points1, 220, 70);
+        this.ctx.fillText(points2, 340, 70);
+    };
+}
+
+function pala(x, y){
+    this.ctx = null;
+    this.width = 10;
+    this.height = 40;
+    this.x_ini = x;
+    this.y_ini = y;
+
+    this.init = function(ctx) {
+        this.ctx = ctx;
+        this.reset();
+    };
+
+    this.draw = function() {
+        this.ctx.fillStyle = 'white';
+        this.ctx.fillRect(this.p_x, this.p_y, this.width, this.height);
+    };
+
+    this.update = function(p_x, p_y) {
+        this.p_x = p_x;
+        this.p_y = p_y;
+    }
+
+    this.reset = function() {
+        this.p_x = this.x_ini;
+        this.p_y = this.y_ini;
+    };
+}
+
+function ball(){
+    this.ctx = null;
+    this.x_ini = 50;
+    this.y_ini = 50;
+    this.width = 5;
+    this.height = 5;
+    this.x = 0;
+    this.y = 0;
+    this.vx = 4;
+    this.vy = 1;
+
+
+    this.init = function(ctx) {
+        this.ctx = ctx;
+        this.reset();
+    };
+
+    this.draw = function () {
+        this.ctx.fillStyle = 'white';
+        this.ctx.fillRect(this.x, this.y, this.width, this.height)
+    };
+
+    this.update = function () {
+        this.x += this.vx;
+        this.y += this.vy;
+        if (this.y > 400 - 5){
+            this.y = 400 - 5;
+            this.vy = this.vy * -1;
+        }
+        if (this.y < 5){
+            this.y = 5;
+            this.vy = this.vy * -1;
+        }
+    };
+
+    this.reset = function() {
+        this.x = this.x_ini;
+        this.y = this.y_ini;
+    }
+}
+
+function main(){
+    var timer = null;
+    var sacar = document.getElementById('sacar');
+    var points1 = 0;
+    var points2 = 0;
+    var p_x1 = 50;
+    var p_y1 = 100;
+    var p_x2 = 550;
+    var p_y2 = 300;
 
     var canvas = document.getElementById('display')
     canvas.width = 600;
@@ -8,98 +107,37 @@ function main()
 
     var ctx = canvas.getContext("2d");
 
-    //-- Raquetas
-    ctx.fillStyle = 'white';
-    ctx.fillRect(50,100, 10, 40)
-    ctx.fillRect(550,300, 10, 40)
+    var campo = new field(canvas.width, canvas.height);
+    var bola = new ball();
+    var player1 = new pala(p_x1, p_y1);
+    var player2 = new pala(p_x2, p_y2);
 
-    //--Red
-    ctx.beginPath();
-    ctx.strokeStyle = "white";
-    ctx.setLineDash([5, 5]);
-    ctx.moveTo(canvas.width/2,0);
-    ctx.lineTo(canvas.width/2, canvas.height)
-    ctx.stroke();
-
-    //-- Puntuación
-    ctx.font = "80px Arial";
-    ctx.fillStyle = 'white';
-    ctx.fillText("0", 220, 70);
-    ctx.fillText("2", 340, 70);
-
-    //-- Definir el objeto BOLA
-    var bola = {
-        //-- Posición inicial de la pelota
-        x_ini: 50,
-        y_ini: 50,
-        //-- Dimensiones de la Bola
-        width: 5,
-        height: 5,
-        //-- Coornenadas
-        x : 0,
-        y : 0,
-        //-- Velocidad
-        vx : 4,
-        vy : 1,
-        //-- Contexto
-        ctx: null,
-        //-- Inicializar la bola
-        init: function(ctx) {
-            this.ctx = ctx;
-            this.reset();
-        },
-        //-- Dibujar
-        draw: function () {
-            this.ctx.fillStyle = 'white';
-            this.ctx.fillRect(this.x, this.y, this.width, this.height)
-        },
-        //-- Update
-        update: function () {
-            this.x += this.vx;
-            this.y += this.vy;
-        },
-        //-- Reset: Set the ball to the initial state
-        reset: function() {
-            this.x = this.x_ini;
-            this.y = this.y_ini;
-        }
-    }
-    //-- Inicializar y pintar la bola
     bola.init(ctx)
+    player1.init(ctx)
+    player2.init(ctx)
+    campo.init(ctx)
     bola.draw()
-    //-- Crear timer para la animación
-    //-- Inicialmente a null
-    var timer = null;
-    //-- Boton de salcar
-    var sacar = document.getElementById('sacar')
-    //-- Función de retrollamda del botón de sacar.
-    //-- ¡Que comience la animación!
+    player1.draw()
+    player2.draw()
+    campo.draw(points1, points2)
+
     sacar.onclick = () => {
-        //-- Si la bola ya se está animando,
-        //-- no hacer nada
         if (!timer) {
-            //-- Lanzar el timer. Su funcion de retrollamada la definimos
-            //-- en su primer parámetro
             timer = setInterval(()=>{
-                //-- Esto se ejecuta cada 20ms
-                //-- Actualizar la bola
                 bola.update();
-                //-- Borrar el canvas
                 ctx.clearRect(0, 0, canvas.width, canvas.height);
-                //-- Dibuar la bola
                 bola.draw();
-                //-- Si la bola llega a la parte derecha del canvas:
-                //-- Terminar
+                player1.draw();
+                player2.draw();
+                campo.draw(points1, points2);
+
                 if (bola.x > canvas.width) {
-                    //-- Eliminar el timer
                     clearInterval(timer)
                     timer = null;
-                    //-- Bola a su posicion inicial
                     bola.reset();
-                    //-- Dibujar la bola en pos. inicial
                     bola.draw();
                 }
-            },20); //-- timer
+            },20);
         }
-    } //-- Fin onclick
+    }
 }
